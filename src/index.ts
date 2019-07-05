c1bc608b8f7bdce9b09717d6d9fb4934bb9ffbd6
kingdaro <kingdaro@gmail.com>
2019-07-05 04:57:42 -0500

    check reminders, send messages when needed

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,34 @@
-import { createBot } from "./bot"
+import { createBot, DiscordBot } from "./bot"
+import { sleep } from "./sleep"
+import { ReminderStorageService } from "./storage"
 
-createBot()
-  .start()
-  .then(() => {
-    console.log("bot is runnin")
-  })
+async function checkReminders(
+  bot: DiscordBot,
+  storage: ReminderStorageService
+) {
+  while (true) {
+    await sleep(1000)
+
+    for (const reminder of await storage.getAll()) {
+      if (Date.now() >= reminder.remindOn) {
+        await storage.remove(reminder.id)
+
+        const sender = await bot.client.fetchUser(reminder.senderId)
+        sender.sendMessage(`hi! here's your reminder: "${reminder.text}"`)
+      }
+    }
+  }
+}
+
+async function main() {
+  const storage = new ReminderStorageService()
+
+  const bot = createBot(undefined, storage)
+  await bot.start()
+
+  console.log("bot is runnin")
+
+  await checkReminders(bot, storage)
+}
+
+main().catch(console.error)
