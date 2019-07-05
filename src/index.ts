d2b8264342eeef5cec6bc8f2405cbf5e9ffec69f
kingdaro <kingdaro@gmail.com>
2019-07-05 04:59:16 -0500

    move reminder checker to its own file

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,7 @@
-import { createBot, DiscordBot } from "./bot"
+import { createBot } from "./bot"
+import { checkReminders } from "./check-reminders"
 import { sleep } from "./sleep"
 import { ReminderStorageService } from "./storage"
-
-async function checkReminders(
-  bot: DiscordBot,
-  storage: ReminderStorageService
-) {
-  while (true) {
-    await sleep(1000)
-
-    for (const reminder of await storage.getAll()) {
-      if (Date.now() >= reminder.remindOn) {
-        await storage.remove(reminder.id)
-
-        const sender = await bot.client.fetchUser(reminder.senderId)
-        sender.sendMessage(`hi! here's your reminder: "${reminder.text}"`)
-      }
-    }
-  }
-}
 
 async function main() {
   const storage = new ReminderStorageService()
@@ -28,7 +11,10 @@ async function main() {
 
   console.log("bot is runnin")
 
-  await checkReminders(bot, storage)
+  while (true) {
+    await checkReminders(bot, storage)
+    await sleep(1000)
+  }
 }
 
 main().catch(console.error)
