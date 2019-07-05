4bd6cf241699055c87e408686e4d685df8f031fe
kingdaro <kingdaro@gmail.com>
2019-07-05 05:13:57 -0500

    checkReminders: accept ReminderStorage, use .send

--- src/check-reminders.ts
+++ src/check-reminders.ts
@@ -1,16 +1,16 @@
 import { DiscordBot } from "./bot"
-import { TestReminderStorage } from "./reminder-storage"
+import { ReminderStorage } from "./reminder-storage"
 
 export async function checkReminders(
   bot: DiscordBot,
-  storage: TestReminderStorage
+  storage: ReminderStorage
 ) {
   for (const reminder of await storage.getAll()) {
     if (Date.now() >= reminder.remindOn) {
       await storage.remove(reminder.id)
 
       const sender = await bot.client.fetchUser(reminder.senderId)
-      sender.sendMessage(`hi! here's your reminder: "${reminder.text}"`)
+      sender.send(`hi! here's your reminder: "${reminder.text}"`)
     }
   }
 }
