399d88c8583f48abb0b99b700adaba47c0b0e436
kingdaro <kingdaro@gmail.com>
2019-08-01 18:51:57 -0500

    collect coverage from all src files

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,8 @@
+// @ts-check
+
+/** @type {jest.InitialOptions} */
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-};
+  preset: "ts-jest",
+  testEnvironment: "node",
+  collectCoverageFrom: [`src/**/*.{ts,tsx}`, `!src/index.ts`],
+}
